118cf8b0bb85126b7243cf7b7c24b1d9f2b961f4
João Luiz Pimenta <joaoluizlopespimenta@gmail.com>
2024-10-13 21:16:48 -0300

    Remove imports não usados

--- src/main/java/com/devops/grababargain/service/customer/ICustomerService.java
+++ src/main/java/com/devops/grababargain/service/customer/ICustomerService.java
@@ -5,8 +5,6 @@ import com.devops.grababargain.model.Customer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
-
 public interface ICustomerService {
     Customer create(Customer data);
 
